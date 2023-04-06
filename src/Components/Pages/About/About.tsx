d370391cd2eaea4e8a516fc2ba9e89a14a9d7d73
Farhah <farhahnizam.work@gmail.com>
2023-04-06 12:29:51 +0800

    added: details and about

--- src/Components/Pages/About/About.tsx
+++ src/Components/Pages/About/About.tsx
@@ -1,9 +1,204 @@
-import React from 'react'
+// // import React from 'react'
 
-const About = () => {
+// // const About = () => {
+// //   return (
+// //     <div>About</div>
+// //   )
+// // }
+
+// // export default About
+
+
+// function About() {
+//   const teamMembers = [
+//     {
+//       name: "Farhah",
+//       position: "CEO",
+//       profilePicture: "",
+//       // profilePicture: "/john-smith.jpg",
+//     },
+//     {
+//       name: "Lubega",
+//       position: "",
+//       profilePicture: "",
+//     },
+//     {
+//       name: "Prem",
+//       position: "",
+//       profilePicture: "",
+//     },
+//     {
+//       name: "Ikmal",
+//       position: "",
+//       profilePicture: "",
+//     },
+//     {
+//       name: "Yuva",
+//       position: "",
+//       profilePicture: "",
+//     },
+//   ];
+
+//   return (
+//     <div>
+//       <About teamMembers={teamMembers} />
+//     </div>
+//   );
+// }
+
+// export default About;
+
+import React from 'react';
+
+function About() {
+
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <h2>Responsive "Meet The Team" Section</h2>
+      <p>Resize the browser window to see the effect.</p>
+      <br />
 
-export default About
+      <div style={{ 
+        display: 'flex',
+        flexWrap: 'wrap'
+      }}>
+
+      <div style={{
+        flex: '33.3%',
+        marginBottom: '16px',
+        padding: '0 8px'
+      }}>
+
+      <div style={{
+        boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)'
+      }}>
+
+      <img src="/w3images/team1.jpg" alt="Farhah" style={{ width: '100%' }} />
+      <div style={{ padding: '0 16px' }}>
+        <h2>Farhah Nizam</h2>
+          <p style={{ color: 'grey' }}>CEO & Founder</p>
+          <p>If I was funny, I would have a good 'about' bio</p>
+          <p>farhah@besquare.com.my</p>
+          <button style={{
+            border: 'none',
+            outline: '0',
+            display: 'inline-block',
+            padding: '8px',
+            color: 'white',
+            backgroundColor: '#000',
+            textAlign: 'center',
+            cursor: 'pointer',
+            width: '100%' 
+      }}> Contact</button>
+      </div>
+      </div>
+      </div>
+      </div>
+
+      <div style={{
+        flex: '33.3%',
+        marginBottom: '16px',
+        padding: '0 8px' }}>
+      <div style={{ boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)' }}>
+      <img src="/w3images/team2.jpg" alt="Lubega" style={{ width: '100%' }} />
+      <div style={{ padding: '0 16px' }}>
+      <h2>Lubega</h2>
+        <p style={{ color: 'grey' }}>Art Director</p>
+        <p>Good stuff</p>
+        <p>example@example.com</p>
+      <button style={{
+        border: 'none',
+        outline: '0',
+        display: 'inline-block',
+        padding: '8px',
+        color: 'white',
+        backgroundColor: '#000',
+        textAlign: 'center',
+        cursor: 'pointer',
+        width: '100%'
+      }}>Contact</button>
+      </div>
+      </div>
+      </div>
+
+      <div style={{
+        flex: '33.3%',
+        marginBottom: '16px',
+        padding: '0 8px' }}>
+      <div style={{ boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)' }}>
+      <img src="/w3images/team2.jpg" alt="Prem" style={{ width: '100%' }} />
+      <div style={{ padding: '0 16px' }}>
+      <h2>Prem</h2>
+        <p style={{ color: 'grey' }}>Art Director</p>
+        <p>Optimus Prem.</p>
+        <p>example@example.com</p>
+      <button style={{
+        border: 'none',
+        outline: '0',
+        display: 'inline-block',
+        padding: '8px',
+        color: 'white',
+        backgroundColor: '#000',
+        textAlign: 'center',
+        cursor: 'pointer',
+        width: '100%'
+      }}>Contact</button>
+      </div>
+      </div>
+      </div>
+
+      <div style={{
+        flex: '33.3%',
+        marginBottom: '16px',
+        padding: '0 8px' }}>
+      <div style={{ boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)' }}>
+      <img src="/w3images/team2.jpg" alt="Ikmal" style={{ width: '100%' }} />
+      <div style={{ padding: '0 16px' }}>
+      <h2>Ikmal</h2>
+        <p style={{ color: 'grey' }}>Art Director</p>
+        <p>I'm sleepy</p>
+        <p>example@example.com</p>
+      <button style={{
+        border: 'none',
+        outline: '0',
+        display: 'inline-block',
+        padding: '8px',
+        color: 'white',
+        backgroundColor: '#000',
+        textAlign: 'center',
+        cursor: 'pointer',
+        width: '100%'
+      }}>Contact</button>
+      </div>
+      </div>
+      </div>
+
+      <div style={{
+        flex: '33.3%',
+        marginBottom: '16px',
+        padding: '0 8px'
+      }}>
+      <div style={{ boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)' }}>
+      <img src="/w3images/team2.jpg" alt="Yuva" style={{ width: '100%' }} />
+      <div style={{ padding: '0 16px' }}>
+        <h2>Yuva</h2>
+          <p style={{ color: 'grey' }}>Art Director</p>
+          <p>I'm shy</p>
+          <p>example@example.com</p>
+        <button style={{
+        border: 'none',
+        outline: '0',
+        display: 'inline-block',
+        padding: '8px',
+        color: 'white',
+        backgroundColor: '#000',
+        textAlign: 'center',
+        cursor: 'pointer',
+        width: '100%' }}>Contact</button>
+      </div>
+      </div>
+      </div>
+      </>
+    )}
+    
+  export default About;
--- src/Components/Pages/Details/Details.tsx
+++ src/Components/Pages/Details/Details.tsx
@@ -1,9 +1,90 @@
-import React from 'react'
+import { getGamesDetails } from "../../APIKey/APIKey";
+import "../../Styles/main.scss";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Id } from "../../APIKey/APIKey";
 
-const Details = () => {
-  return (
-    <div>Details</div>
-  )
+// interface GameDetails {
+//   name?: { id: string }[];
+//   metacritic?: { id: number };
+//   ratings?: { id: string };
+//   esrb?: { id: string };
+//   genres?: { id: string };
+//   released?: { id: string };
+//   playtime?: { id: string };
+//   description?: { id: string };
+// }
+
+interface GameDetails {
+  name?: string;
+  metacritic?: number;
+  ratings?: string;
+  esrb?: string;
+  genres?: string;
+  released?: string;
+  playtime?: string;
+  description?: string;
 }
 
-export default Details
+function Details() {
+  const [gameDetails, setGameDetails] = useState<GameDetails>({});
+  const { id } = useParams<{ id: string }>();
+
+  const GameData = (id: string | undefined) => {
+    if (id) {
+      const gameId: Id = { id: parseInt(id) };
+      getGamesDetails(gameId).then((output) => {
+        setGameDetails(output?.data?.results);
+      });
+    }
+  };
+  
+
+  useEffect(() => {
+    GameData(id);
+  }, [id]);
+
+  return (
+    <div>
+      <div>Game Details</div>
+      <table className="text">
+        <tbody>
+          <tr>
+            <th>Title</th>
+            <td>{gameDetails?.name}</td>
+          </tr>
+          <tr>
+            <th>Metacritic</th>
+            <td>{gameDetails?.metacritic}</td>
+          </tr>
+          <tr>
+            <th>Ratings</th>
+            <td>{gameDetails?.ratings}</td>
+          </tr>
+          <tr>
+            <th>Esrb</th>
+            <td>{gameDetails?.esrb}</td>
+          </tr>
+          <tr>
+            <th>Genres</th>
+            <td>{gameDetails?.genres}</td>
+          </tr>
+          <tr>
+            <th>Released</th>
+            <td>{gameDetails?.released}</td>
+          </tr>
+          <tr>
+            <th>Playtime</th>
+            <td>{gameDetails?.playtime}</td>
+          </tr>
+          <tr>
+            <th>Description</th>
+            <td>{gameDetails?.description}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Details;
