1061beac5053463c9cfa87fbfc9f7802083efb7c
yuvarajelamko <yuvaraj@besquare.com.my>
2023-04-06 16:55:09 +0800

    added : signin, signout

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import AuthModal from '../Login/AuthModal';
+import AuthDetails from '../Login/AuthDetails';
 
 const Navbar = () => {
   return (
@@ -8,6 +10,10 @@ const Navbar = () => {
         <span><h2><Link to='/genres'>Genre</Link></h2></span>
         <span><h2><Link to='/about'>About</Link></h2></span>
         <span><h2><Link to='/account'>Account</Link></h2></span>
+        <AuthModal/>
+        <AuthDetails/>
+        
+
 </div>
   )
 }
