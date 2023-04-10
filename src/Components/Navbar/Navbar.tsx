435ed85b0ad2eb570b403069926754821876f8b3
prembesquare <prem@besquare.com.my>
2023-04-10 10:10:12 +0800

    responsive changes

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -4,12 +4,18 @@ import AuthModal from '../Login/AuthModal';
 import AuthDetails from '../Login/AuthDetails';
 
 const Navbar = () => {
+  const handleShift = () => {
+    window.scrollTo({
+      top:1200,
+      behavior: 'smooth'
+    });
+  }
   return (
     <div className='nav'>
-        <h2><Link to='/' >Home</Link></h2>
-        <h2><Link to='/genres#genreidsec'>Genre</Link></h2>
-        <h2><Link to='/about'>About</Link></h2>
-        <h2><Link to='/account'>Account</Link></h2>
+        <h2><Link to='/' onClick={handleShift}>Home</Link></h2>
+        <h2><Link to='/genres' onClick={handleShift}>Genre</Link></h2>
+        <h2><Link to='/about' onClick={handleShift}>About</Link></h2>
+        <h2><Link to='/account' onClick={handleShift}>Account</Link></h2>
         <AuthModal/>
         <AuthDetails/>
         
